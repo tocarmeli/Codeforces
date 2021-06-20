45ba03ec458a87bd21eccd371584cb614a155159
Tomer C <50640826+tocarmeli@users.noreply.github.com>
2021-06-20 13:42:19 -0400

    Finished divisibility.c

--- 800Diff/divisibility.c
+++ 800Diff/divisibility.c
@@ -1,24 +1,32 @@
+// Problem 1328A
+// https://codeforces.com/problemset/problem/1328/A
+
 #include <stdio.h>
 
 int main(){
-    int testCases;
-    int firstInp;
-    int secondInp;
-    int i;
-    int moves = 0;
+    int t;
+    long long int a;
+    long long int b;
+    int totalSteps;
 
-    scanf("%d", &testCases);
+    scanf("%d", &t);
 
-    for (i = 0; i < testCases; i++){
-        moves = 0;
-        scanf("%d%d", &firstInp, &secondInp);
-
-        if (firstInp % secondInp == 0){
-            moves = 0;
-            printf("%d", moves);
+    for (int k = 0; k < t; k++){
+        totalSteps = 0;
+        scanf("%lld %lld", &a, &b);
+        /*while (a % b != 0){
+            a++;
+            totalSteps++;
+        }*/
+        if (a % b != 0){
+            totalSteps = b - (a % b);
+            printf("%d\n", totalSteps);
+        } else {
+            printf("%d\n", 0);
         }
+        
     }
-
+    
 
     return 0;
 }

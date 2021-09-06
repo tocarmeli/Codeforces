e8bb54358a29246d16dd206dee5fa41e02c24233
Tomer C <50640826+tocarmeli@users.noreply.github.com>
2021-09-06 18:44:26 -0400

    Uploaded sale.c

---
+++ 900Diff/sale.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(){
+    int n, m, price;
+    int maxPrice = 0;
+    scanf("%d%d", &n, &m);
+    int values[n];
+
+    for (int k = 0; k < n; k++){
+        scanf("%d", &price);
+        price *= -1;
+        values[k] = price;
+    }
+    for (int z = 1; z < n; z++){
+            for (int j = 0; j < z; j++){
+                if (values[j]> values[z]){
+                    int prev = values[j];
+                    values[j] = values[z];
+                    values[z] = prev;
+                }
+            }
+            
+    }
+    if (values[n - m] < 0){
+        for (int a = n - m + 1; a < n; a++){
+            if (values[a] >= 0){
+                maxPrice += values[a];
+            }
+        }
+    } else {
+        for (int f = n - m; f < n; f++){
+            maxPrice += values[f];
+        }
+    }
+    
+    printf("%d\n", maxPrice);
+
+
+    return 0;
+}
